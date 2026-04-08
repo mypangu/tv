79eaade2ee41b9f2578036889d91d6cd199da75a
MrPangu <235312546+mypangu@users.noreply.github.com>
2026-04-08 14:55:56 +0530

    0804261455

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // ===============================
 
 // Change this whenever you update UI files
-const CACHE_VERSION = "0804261040";
+const CACHE_VERSION = "0804261455";
 const STATIC_CACHE = "pangu-static-" + CACHE_VERSION;
 
 // Static assets ONLY (never include JSON or video files)
